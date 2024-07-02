b55ac7cbf70352e694d42353b7b9893d97cf0250
B. Sai Bhaskar <bhamidipati.2022ug1016@iiitranchi.ac.in>
2024-07-03 02:35:03 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+  import * as Menubar from "$lib/components/ui/menubar";
+</script>
+  <Menubar.Root class='m-0 p-0 border-[0px] flex justify-end bg-gradient-to-r from-purple-500 to-pink-500 h-14'>
+    <Menubar.Menu>
+      <Menubar.Item class='w-full font-bebas text-xl antialiased tracking-wider'><span class="font-dosis text-2xl text-red-700">M</span>AIL.<span class="font-dosis text-2xl text-red-700">M</span>E</Menubar.Item>
+    </Menubar.Menu>
+    <Menubar.Menu >
+      <Menubar.Trigger class='font-dosis  text-lg'>
+        <a data-sveltekit-preload-data="tap" href="/auth/signup">
+        SignUp
+      </a>
+    </Menubar.Trigger>
+    </Menubar.Menu>
+    <Menubar.Menu >
+      <Menubar.Trigger class='font-dosis  text-lg'>
+        <a data-sveltekit-preload-data="tap" href="/auth">
+          SignIn
+        </a>
+      </Menubar.Trigger>
+    </Menubar.Menu>
+  </Menubar.Root>
+<div class="bg-[url('/src/lib/images/joe-woods-4Zaq5xY5M_c-unsplash(2).jpg')] w-full h-[100vh] flex flex-col  ">
+    <div>
+      <h1 class="font-wireone text-[100px]"> Let's get Started with....</h1>
+    </div>
+    <div class="w-full text-[130px] text-center">
+      <h2><span class="font-dosis text-[150px] text-red-700">M</span>AIL.<span class="font-dosis text-[150px] text-red-700">M</span>E</h2>
+    </div>
+</div>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+    import * as Card from "$lib/components/ui/card/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+  </script>
+<div>
+    
+<div class="w-full h-[100vh] flex justify-center align-middle flex-wrap bg-[#090909]">   
+  <div class="w-full text-center text-5xl"><h1 class="text-white"><span class="font-dosis text-6xl text-red-700">M</span>AIL.<span class="font-dosis text-6xl text-red-700">M</span>E</h1></div>
+  <div class="h-[80vh]">
+    <Card.Root class="mx-auto max-w-sm">
+    <Card.Header>
+      <Card.Title class="text-2xl">Login</Card.Title>
+      <Card.Description>Enter your email below to login to your account</Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <div class="grid gap-4">
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div class="grid gap-2">
+          <div class="flex items-center">
+            <Label for="password">Password</Label>
+            <a href="##" class="ml-auto inline-block text-sm underline">
+              Forgot your password?
+            </a>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" class="w-full">Login</Button>
+        <Button variant="outline" class="w-full">Login with Google</Button>
+      </div>
+      <div class="mt-4 text-center text-sm">
+        Don&apos;t have an account?
+        <a href="##" class="underline"> Sign up </a>
+      </div>
+    </Card.Content>
+  </Card.Root>
+</div>
+</div>
+</div>
